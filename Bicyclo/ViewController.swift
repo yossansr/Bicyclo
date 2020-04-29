d4f0175000d61f5c7f202469b4ea140ba17969fe
Yossan Rahmadi <yossansr06@gmail.com>
2020-04-29 12:46:01 +0800

    Initial Commit

---
+++ Bicyclo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bicyclo
+//
+//  Created by Yossan Rahmadi on 29/04/20.
+//  Copyright © 2020 Yossan Rahmadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
